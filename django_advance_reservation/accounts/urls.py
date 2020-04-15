3ed753adcfdb160144e07825f3538afd8ab64d25
DongyeopGu <gdy6650@gmail.com>
2020-04-15 21:33:06 +0900

    add urls

--- django_advance_reservation/accounts/urls.py
+++ django_advance_reservation/accounts/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('application/', views.application, name="application"),
-    path('<int:pk>/update/', views.update, name="update"),
-    path('<int:pk>/delete/', views.delete, name="delete"),
-    path('<int:pk>/', views.detail, name="detail"),
-    path('reservation/', include('reservation.urls')),
+    path('application/', views.application, name="application"),    # 신청 페이지
+    path('<int:pk>/update/', views.update, name="update"),          # 신청 정보 수정
+    path('<int:pk>/delete/', views.delete, name="delete"),          # 신청 정보 삭제
+    path('<int:pk>/', views.detail, name="detail"),                 # 신청 정보 확인
+    path('reservation/', include('reservation.urls')),              # 
 ]
--- django_advance_reservation/reservation/urls.py
+++ django_advance_reservation/reservation/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = "reservation"
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('accounts/', include('accounts.urls'))
+    path('', views.index, name="index"),            # 메인 페이지
+    path('board/', views.board, name="board"),      # 고구마 정보를 보여주기 위해
+    path('accounts/', include('accounts.urls')),    # 신청 url로 넘어갈 수 있도록
 ]
