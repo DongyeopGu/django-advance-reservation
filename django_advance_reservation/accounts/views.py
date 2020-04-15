38df0e69dea172e1de406e8f5dd556f0de203c4b
DongyeopGu <gdy6650@gmail.com>
2020-04-15 21:37:04 +0900

    views 큰 틀

--- django_advance_reservation/accounts/views.py
+++ django_advance_reservation/accounts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def application(request):
+    pass
+
+def update(request, pk):
+    pass
+
+def delete(request, pk):
+    pass
+
+def detail(request, pk):
+    pass
--- django_advance_reservation/reservation/views.py
+++ django_advance_reservation/reservation/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):     # 메인 페이지에 가장 먼저 보여줄 것들
+    pass
+
+def board(request):     # 필요한 사진과 정보를 올려둘 board
+    pass
+
+def notice(request):    # 공지사항을 위해
+    pass
