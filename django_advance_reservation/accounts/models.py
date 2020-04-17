681fb519e5e0c81a7c7d5a28d701322d1c3739f4
DongyeopGu <gdy6650@gmail.com>
2020-04-17 17:14:26 +0900

    user model customizing(1)

--- django_advance_reservation/accounts/models.py
+++ django_advance_reservation/accounts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser # AbstractUser를 가져옴
 
-# Create your models here.
+class User(AbstractUser): # User 모델에 추가할 필드를 넣기 위해 AbstractUser 상속받음
+    email_address = models.EmailField()
+    sweetpotato_size = models.CharField(max_length=30)
+    sweetpotato_num = models.IntegerField()
--- django_advance_reservation/reservation/models.py
+++ django_advance_reservation/reservation/models.py
@@ -1,4 +1,3 @@
 from django.db import models
 
 # 사진 및 정보를 업로드 하기 위한 모델 정의
-
